46bea00b97bed77ecbf20711786746d6b82bfa3f
manuel peña <manpenap@gmail.com>
2023-12-24 00:27:17 -0300

    blog de guia masculina

---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Consejos de Seducción',
+	'Sexualidad y Erotismo',
+	'Crecimiento Personal',
+	'Análisis de Relaciones'
+	
+] as const
