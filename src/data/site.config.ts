5f93b4967b56f791a2a9ed0fec33d664717e6ff5
manuel peña <manpenap@gmail.com>
2023-12-26 16:38:09 -0300

    cambios en etiquetas y robot.txt

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'ClicExitoso', // Site author
+	author: 'Guía Masculina', // Site author
 	title: 'Guía Masculina para el Amor', // Site title.
-	description: 'Toda la orientación que los hombres necesitan para navegar con éxito el mundo del amor y las relaciones. Descubre consejos de citas efectivas, estrategias para el desarrollo personal en el ámbito romántico, claves para mantener relaciones saludables y exploraciones en la psicología del amor. Encuentra tu camino hacia conexiones más significativas y relaciones duraderas con la guía experta para hombres en el amor.', // Description to display in the meta tags
+	description:
+		'Toda la orientación que los hombres necesitan para navegar con éxito el mundo del amor y las relaciones. Descubre consejos de citas efectivas, estrategias para el desarrollo personal en el ámbito romántico, claves para mantener relaciones saludables y exploraciones en la psicología del amor. Encuentra tu camino hacia conexiones más significativas y relaciones duraderas con la guía experta para hombres en el amor.', // Description to display in the meta tags
 	lang: 'es_US',
 	ogLocale: 'es_US',
 	shareMessage: 'Compartir', // Message to share a post on social media
